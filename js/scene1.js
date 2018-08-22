b1e7046de32ef08de878e330b7e0dc5060052b9a
icyJoseph <sephxd1234@gmail.com>
2018-08-22 08:28:53 +0200

    feat: move to click, A and D keys

--- js/scene1.js
+++ js/scene1.js
@@ -11,7 +11,20 @@ class Scene1 extends Phaser.Scene {
 
   create() {
     this.image = this.add.image(400, 300, "GFS");
+
+    this.input.keyboard.on("keyup_D", () => (this.image.x += 10));
+
+    this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+
+    this.input.on("pointerdown", event => {
+      this.image.x = event.x;
+      this.image.y = event.y;
+    });
   }
 
-  update() {}
+  update(delta) {
+    if (this.key_A.isDown) {
+      this.image.x--;
+    }
+  }
 }
